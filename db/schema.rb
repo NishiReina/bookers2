cc8759c4113b42daa71ece6acf2d108fe035434b
NishiReina <nishi.reina.infratop@gmail.com>
2021-05-05 04:50:23 +0000

    [Finishi] assignment4

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_074019) do
+ActiveRecord::Schema.define(version: 2021_05_03_122518) do
 
   create_table "add_profile_image_id_to_users", force: :cascade do |t|
     t.string "profile_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "book_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -22,6 +30,13 @@ ActiveRecord::Schema.define(version: 2021_04_30_074019) do
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
